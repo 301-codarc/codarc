62dee8b09dd9d62b1afa3bef256c9af6689379a4
Timothy Carol Sarkar Orgha <carolbro@Timothys-MacBook-Pro-2.local>
2025-09-26 12:29:40 -0600

    Timothy's Pentagon

---
+++ app/src/main/java/com/example/codarc/Pentagon.java
@@ -0,0 +1,15 @@
+package com.example.codarc;
+
+public class Pentagon {
+    private int side;
+
+    public Pentagon(int x, int y, int side) {
+        super(x, y);
+        this.side = side;
+    }
+
+    public int getSide() {
+        return side;
+    }
+}
+}
