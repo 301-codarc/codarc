fe614f584eec9fc3ccdc799a56ed959a5fd1ae60
PhuNguyen16 <108025921+PhuNguyen16@users.noreply.github.com>
2025-09-26 12:26:41 -0600

    Changed colour

--- app/src/main/java/com/example/codarc/Shape.java
+++ app/src/main/java/com/example/codarc/Shape.java
@@ -3,7 +3,7 @@ package com.example.codarc;
 public abstract class Shape {
     int x;
     int y;
-    private String color = "blue";
+    private String color = "cerulean";
 
     public Shape(int x, int y) {
         this.x = x;
