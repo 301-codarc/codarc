12f5cd736ea997d42864198964f7719616ec7abf
aurko616 <mazumde1@ualberta.ca>
2025-09-26 13:07:57 -0600

    Update Shape.java
    changed color

--- app/src/main/java/com/example/codarc/Shape.java
+++ app/src/main/java/com/example/codarc/Shape.java
@@ -3,7 +3,7 @@ package com.example.codarc;
 public abstract class Shape {
     int x;
     int y;
-    private String color = "purple";
+    private String color = "black";
 
     public Shape(int x, int y) {
         this.x = x;
