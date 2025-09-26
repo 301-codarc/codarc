2fdb493da598befa89b77990e48182744f243282
salmananur <180611830+salmananur@users.noreply.github.com>
2025-09-26 11:26:49 -0600

    idk really

--- app/src/main/java/com/example/codarc/Rectangle.java
+++ app/src/main/java/com/example/codarc/Rectangle.java
@@ -3,6 +3,7 @@ package com.example.codarc;
 public class Rectangle extends Shape {
     private int width;
     private int height;
+    // Just to commit
 
     public Rectangle(int x, int y, int width, int height) {
         super(x, y);
