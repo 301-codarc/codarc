ab9a7e5e5a6a10ea2ed3c14ab1faaa3469f4ba7e
Timothy Carol Sarkar Orgha <carolbro@Timothys-MacBook-Pro-2.local>
2025-09-26 13:15:39 -0600

    Trying red color

--- app/src/main/java/com/example/codarc/Shape.java
+++ app/src/main/java/com/example/codarc/Shape.java
@@ -3,7 +3,7 @@ package com.example.codarc;
 public abstract class Shape {
     int x;
     int y;
-    String color = "blue";
+    String color = "red";
 
     public Shape(int x, int y) {
         this.x = x;
