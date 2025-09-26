f69730c1846f12120cf0f25b449ff0add0396b7c
Aurko Mazumder <aurkomazumder@Aurkos-MacBook-Pro.local>
2025-09-26 13:09:55 -0600

    Added color field = blue to Shape locally

--- app/src/main/java/com/example/codarc/Shape.java
+++ app/src/main/java/com/example/codarc/Shape.java
@@ -3,7 +3,8 @@ package com.example.codarc;
 public abstract class Shape {
     int x;
     int y;
-    private String color = "red";
+    private String color = "blue";
+
 
     public Shape(int x, int y) {
         this.x = x;
